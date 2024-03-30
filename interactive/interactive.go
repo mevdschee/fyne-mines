d17dee93f1c20fabfd546d80b655b67be50245a3
Maurits van der Schee <maurits@vdschee.nl>
2024-03-30 23:14:14 +0100

    mouse down and mouse up

--- interactive/interactive.go
+++ interactive/interactive.go
@@ -12,12 +12,17 @@ import (
 type Image struct {
 	*canvas.Image
 	name        string
+	isDown      bool
 	OnMouseDown func()
 	OnMouseUp   func()
 }
 
+// ensure Mousable and Hoverable
+var _ desktop.Mouseable = (*Image)(nil)
+var _ desktop.Hoverable = (*Image)(nil)
+
 func NewImage(image *canvas.Image, name string) *Image {
-	return &Image{image, name, nil, nil}
+	return &Image{image, name, false, nil, nil}
 }
 
 func (i *Image) CreateRenderer() fyne.WidgetRenderer {
@@ -27,9 +32,13 @@ func (i *Image) CreateRenderer() fyne.WidgetRenderer {
 func (i *Image) MouseDown(ev *desktop.MouseEvent) {
 	log.Println("mouse-down: " + i.name)
 	log.Printf("mouse-down: %v\n", ev)
+	//if ev.Button = desktop.MouseButtonPrimary {} // left
+	//if ev.Button = desktop.MouseButtonSecondary {} // right
+	//if ev.Button = desktop.MouseButtonTertiary {} // middle
 	if i.OnMouseDown != nil {
 		i.OnMouseDown()
 	}
+	i.isDown = true
 }
 
 func (i *Image) MouseUp(ev *desktop.MouseEvent) {
@@ -38,4 +47,21 @@ func (i *Image) MouseUp(ev *desktop.MouseEvent) {
 	if i.OnMouseUp != nil {
 		i.OnMouseUp()
 	}
+	i.isDown = false
+}
+
+func (i *Image) MouseIn(ev *desktop.MouseEvent) {
+	//log.Println("Mouse In")
+}
+
+func (i *Image) MouseOut() {
+	//log.Println("Mouse Out")
+	if i.isDown {
+		i.MouseUp(&desktop.MouseEvent{})
+	}
+}
+
+func (i *Image) MouseMoved(ev *desktop.MouseEvent) {
+	//log.Println("Mouse Moved")
+	//log.Printf("Mouse Moved %v\n", ev.Button)
 }
